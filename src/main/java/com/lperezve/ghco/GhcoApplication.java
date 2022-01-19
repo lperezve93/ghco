e520b19658a21acff34684f4fb6a734476229b5e
lperezve93lperezve93lperezve <lorenap.uk93@gmail.com>
2022-01-19 18:43:33 +0000

    Initial commit

---
+++ src/main/java/com/lperezve/ghco/GhcoApplication.java
@@ -0,0 +1,13 @@
+package com.lperezve.ghco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhcoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lperezve/ghco/GhcoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lperezve.ghco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
